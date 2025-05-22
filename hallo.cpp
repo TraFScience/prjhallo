243dd65ad69b1aeb16a34eb12e8fd5bd5466bb8c
Ahmad Nabil Avila <nabilavila050@gmail.com>
2025-05-22 18:37:16 +0800

    menampilkan data pada array

--- hallo.cpp
+++ hallo.cpp
@@ -6,13 +6,30 @@ using namespace std;
 int main()
 {
     WM();
+
+    cout << "=====================================\n";
+    cout << "PROGRAM INPUT NAMA\n";
+    cout << "=====================================\n";
    
-    string nama [5];
+    string nama[5];
     for (int i = 0; i<5; i++)
     {
         cout << "Masukkan Nama Ke- " << i+1 << ":";
         getline(cin,nama[i]);
     }
 
+    cout <<"\n";
+    cout << "=====================================\n";
+    cout << "Daftar Nama Yang Telah Diinputkan : \n";
+    cout << "=====================================\n";
+    cout << "\n";
+
+    for (int i = 0; i<5; i++)
+    {
+        cout << "Nama Ke- " << i+1 << ": " << nama[i] << "\n";
+    }
+
+    cout << "\n";
+    
     return 0;
 }
