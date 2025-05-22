10dd44eb7a7bcbc666a9d3597bfe7a2e90166322
Ahmad Nabil Avila <nabilavila050@gmail.com>
2025-05-22 18:21:49 +0800

    memasukan data 5 buah nama  ke dalam array string

--- hallo.cpp
+++ hallo.cpp
@@ -6,10 +6,13 @@ using namespace std;
 int main()
 {
     WM();
+   
     string nama [5];
-    
-    
-    
-    
+    for (int i = 0; i<5; i++)
+    {
+        cout << "Masukkan Nama Ke- " << i+1 << ":";
+        getline(cin,nama[i]);
+    }
+
     return 0;
 }
